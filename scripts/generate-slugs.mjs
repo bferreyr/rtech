556c357238cf4498ab78535e3f2ddc0d224e607a
bferreyr <bartu@ngamers.net>
2026-07-24 11:17:56 -0300

    fix: instantiate PrismaClient with PrismaPg adapter

--- scripts/generate-slugs.mjs
+++ scripts/generate-slugs.mjs
@@ -1,7 +1,12 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
+import pkg from 'pg';
+const { Pool } = pkg;
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient();
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
     console.log("Fetching products...");
